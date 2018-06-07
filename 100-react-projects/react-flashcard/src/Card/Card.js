bcf9fc8f8c7e2bfdcf2d1ddadbebd7905a6e0327
Ester Gomes <estergomesdesign@gmail.com>
2018-06-07 11:21:22 -0300

    flash card app done

--- 100-react-projects/react-flashcard/src/Card/Card.js
+++ 100-react-projects/react-flashcard/src/Card/Card.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Card.css'
 
 const Card = (props) => (
-  <div className="card-container">
-    <div className="card">
-      <div className="front">
-        <div className="eng">English word</div>
-      </div>
-      <div className="back">
-        <div className="han">Hanzi</div>
-        <div className="pin">Pin</div>
-      </div>
+    <div className="card-container">
+        <div className="card">
+            <div className="front">
+                <div className="eng">{ props.eng }</div>
+            </div>
+            <div className="back">
+                <div className="han">{ props.han }</div>
+                <div className="pin">{ props.pin }</div>
+            </div>
+        </div>
     </div>
-  </div>
 )
 
 export default Card;
---
+++ 100-react-projects/react-flashcard/src/Config/db_config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyC2W0LQH2wAvncR0BWbbxckuAskHBDLef4",
+    authDomain: "react-flashback.firebaseapp.com",
+    databaseURL: "https://react-flashback.firebaseio.com",
+    projectId: "react-flashback",
+    storageBucket: "",
+    messagingSenderId: "530004645181"
+}
